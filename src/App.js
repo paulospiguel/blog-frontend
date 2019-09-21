3f3fc39dc57745f374a8a18bf551050756d5ce30
Paulo Spiguel <paulospil@hotmail.com>
2019-09-21 19:12:39 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+
+function App() {
+ 
+const mudarCor = () => {
+  const obj = document.getElementById("color");
+    obj.className === "red" ? obj.classList.replace("red","white") :  obj.classList.replace("white", "red");
+ 
+  }; 
+
+  const typeWriter = () =>{
+    const elemento = document.querySelector(".titulo-principal");
+    const textoArray = elemento.innerHTML.split("");
+    elemento.innerHTML = " ";
+    
+    textoArray.forEach(function(letra, i){   
+      
+      setTimeout(function(){
+          elemento.innerHTML += letra;
+        }, 75 * i)
+      });
+    
+  }
+
+  setInterval(typeWriter, 10000);
+  setInterval(mudarCor, 1000)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          <strong id="color" className="red">Blog </strong>
+          do Paulo Spiguel
+        </h1>
+        <p className="titulo-principal">Em breve estarei publicado novidades. Fique ligado ...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
